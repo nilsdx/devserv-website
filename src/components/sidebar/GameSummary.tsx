8c0661acee4aa3ed5bae065003cdb64222125be3
nilsdx <nils.durieux@gmail.com>
2025-11-23 19:06:47 +0100

    game info is now a dropdown

--- src/components/sidebar/GameSummary.tsx
+++ src/components/sidebar/GameSummary.tsx
@@ -1,5 +1,7 @@
+'use client'
+
 import { PSOClient, PSOGame } from "@/src/types/server-summary";
-import React from "react";
+import React, { useState } from "react";
 
 interface GameSummaryProps {
     game: PSOGame
@@ -7,13 +9,26 @@ interface GameSummaryProps {
 }
 
 const GameSummary: React.FC<GameSummaryProps> = ({game, party}) => {
+    const [open, setOpen] = useState<boolean>(false);
+
     return (
-        <div className="border-2 border-white">
-            <p>Game name: {game.Name}</p>
-            <p>{game.QuestInProgress ? game.Quest : '*Free roam*'}</p>
-            {party.map((c) => (
-                <p key={c.Name}>{c.Name} - Lv{c.Level}</p>
-            ))}
+        <div className="border border-white">
+            <button
+                onClick={() => setOpen(o => !o)}
+                className="text-left w-full"
+            >
+                {game.Name}
+            </button>
+
+            {open && (
+                <div className="mt-1">
+                    <p>{game.QuestInProgress ? game.Quest : "*Free roam"}</p>
+
+                    {party.map(c => (
+                        <p key={c.ID}>{c.Name} - Lv{c.Level}</p>
+                    ))}
+                </div>
+            )}
         </div>
     )
 }
